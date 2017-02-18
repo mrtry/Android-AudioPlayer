568e6b493c7af5f1a2c4cd467c446a6e941c1f08
mrtry <yamamoto9302@gmail.com>
2017-02-18 18:13:35 +0900

    MainActivityからMainFragmentを読み込むようにした

--- app/src/main/java/com/mrtry/mediaplayer/MainActivity.java
+++ app/src/main/java/com/mrtry/mediaplayer/MainActivity.java
@@ -1,7 +1,11 @@
 package com.mrtry.mediaplayer;
 
+import android.app.FragmentTransaction;
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mrtry.mediaplayer.fragments.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainFragment mainFragment = new MainFragment();
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        transaction.add(R.id.container, mainFragment);
+        transaction.commit();
+
     }
 }
